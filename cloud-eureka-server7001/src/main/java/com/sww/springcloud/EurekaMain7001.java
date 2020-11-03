f47d863c741e22df5a203953186bd85a9469f497
sunweiwen <850062135@qq.com>
2020-11-03 11:37:51 +0800

    Eureka服务注册与发现---单机Eureka构建

--- cloud-eureka-server7001/src/main/java/com/sww/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/sww/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.sww.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Aeiherumuh
- * @date 2020/10/14
+ * @date 2020/11/3
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-            SpringApplication.run(PaymentMain8001.class, args);
-        }
+        SpringApplication.run(EurekaMain7001.class);
+    }
 }
